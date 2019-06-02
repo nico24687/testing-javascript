f283e249b96a4926fe3adc6d76a427f215174d11
nico24687 <theonlynico@hotmail.com>
2019-06-02 15:39:23 -0600

    finding longest word

--- implementation.js
+++ implementation.js
@@ -1,9 +1,35 @@
-// Here is an example to run tests IN YOUR BROWSER.
-
 function square(n) {
   return n * n;
 }
 
+function addTwo(n) {
+  return n + 2
+}
 
-// Here is an example to run tests with the command `npm test` IN YOUR TERMINAL
-// module.exports = square
+function multiply(n,m){
+  return n * m  
+}
+
+function subtract(n,m){
+  return n - m 
+}
+
+function divide(n,m){
+  return n / m 
+}
+
+function reverseString(word){
+  return word.split("").reverse().join("")
+}
+
+function findLongestWord(sentance){
+  words_array = sentance.split(" ")
+  longest_word = words_array.reduce(function(a,b){ return a.length > b.length ? a:b })
+  return longest_word
+
+}
+
+
+module.exports = {square, addTwo, multiply, subtract, divide, reverseString, findLongestWord}
+
+
--- test.js
+++ test.js
@@ -1,9 +1,45 @@
-// Uncomment the following lines to execute `npm test` in your terminal.
-// var assert = require('chai').assert;
-// var square = require('./implementation');
+var assert = require('chai').assert;
+
+var myFunctions = require('./implementation');
 
 describe('square', function () {
   it('returns the square of a number', function () {
-    assert.equal(square(2), 4);
+    assert.equal(myFunctions.square(2), 4);
   });
 });
+
+describe('addTwo', function(){
+  it('adds two to a given number', function(){
+    assert.equal(myFunctions.addTwo(4), 6)
+  })
+})
+
+describe('multiply', function(){
+  it('adds two numbers together', function(){
+    assert.equal(myFunctions.multiply(4,2), 8)
+  })
+})
+
+describe("subtract", function(){
+  it("subtracts one number from another", function(){
+    assert.equal(myFunctions.subtract(7,1), 6)
+  })
+})
+
+describe("divide", function(){
+  it("divides one number by another", function(){
+    assert.equal(myFunctions.divide(10,2), 5)
+  })
+})
+
+describe("reverseString", function(){
+  it("reverses the letters of a string", function(){
+    assert.equal(myFunctions.reverseString("hello"), "olleh")
+  })
+})
+
+describe("findLongestWord", function(){
+  it("returns the longest word in a sentance", function(){
+    assert.equal(myFunctions.findLongestWord('the cat sat on the piano'), "piano")
+  })
+})
